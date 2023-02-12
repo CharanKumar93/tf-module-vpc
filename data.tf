98daba18c532163ca260e7a7cb786fd6f8696a6e
Charan <gcharankumar99@gmail.com>
2023-02-12 14:11:06 -0600

    vars file

--- data.tf
+++ data.tf
@@ -1,5 +1,1 @@
-resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
-}
+data "aws_caller_identity" "current" {}
