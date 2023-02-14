1fd5de64c9abbb030b82e5d2fa6d2618ccb3cef0
Charan <gcharankumar99@gmail.com>
2023-02-14 00:02:30 -0600

    vars file

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block                = "data.aws_vpc.default.cidr_block"
+    cidr_block                = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
