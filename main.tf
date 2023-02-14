a9771f6b34124dbf86c9aaf99be59356066cd1f9
Charan <gcharankumar99@gmail.com>
2023-02-13 20:54:37 -0600

    vars file

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count      = length(public_subnets_cidr)
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
   tags       = merge(
     local.common_tags,
@@ -18,9 +18,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count      = length(private_subnets_cidr)
+  count      = length(var.private_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
+  cidr_block = var.private_subnets_cidr[count.index]
 
   tags       = merge(
     local.common_tags,
@@ -58,7 +58,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block                = "data.aws_vpc.default.cidr_block"
-    vpc_peering_connection_id = aws_aws_vpc_peering_connection.peer.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   tags       = merge(
@@ -99,7 +99,7 @@ resource "aws_route_table" "private" {
   route {
     cidr_block                = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
-      }
+  }
 
   tags       = merge(
     local.common_tags,
